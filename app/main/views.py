6fa56316b223c3d562905462512119497fe65091
mahan-noor <ismahanabey@gmail.com>
2021-04-19 11:24:40 +0300

    restructure config.py file

--- app/main/views.py
+++ app/main/views.py
@@ -14,5 +14,5 @@ def index():
     View root page function that returns the index page and its data
     '''
 
-    Genaral_news
-    return render_template('index.html',message = message)
+    
+  
--- config.py
+++ config.py
@@ -1,7 +1,22 @@
-class Config:
-    '''
-    General configuration parent class
-    '''
+import os
+   
+class config:
+
     SOURCE_BASE_URL = 'https://newsapi.org/v2/sources{}?apiKey={}'
     ARTICLE_BASE_URL = ' https://newsapi.org/v2/everything?q=Apple&from=2021-04-18&sortBy=popularity&apiKey={}'
-    NEWS_API_KEY = '315048c7f545435692ee48d4e32a3d71'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
+
